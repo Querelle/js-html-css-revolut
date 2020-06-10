8c433a4df1a866dc99e5423ee1e7fa55d95c0a56
Andrea De Brest <persona.qdb@gmx.com>
2020-06-10 20:12:05 +0200

    aggiunti commenti

--- js/script.js
+++ js/script.js
@@ -1,22 +1,26 @@
 $(document).ready(
   function() {
+
+    // mostrare dropdown
     $('.hover').mouseenter(
       function() {
         $(this).children('.dropdown-container').show();
       });
 
+    // nascondere dropdown
     $('.hover').mouseleave(
       function() {
         $(this).children('.dropdown-container').hide();
       });
 
+    // toggle dropdown al click
     $('.hover').click(
       function() {
         $('.dropdown-container').toggle();
       }
     )
 
-
+    // rendere il testo bold al passaggio del mouse
     $('li').mouseenter(
       function() {
           $(this).addClass('bold');
